6599e568b670f68c3682a71e2abdd45b045daf01
CLEVER <sanya.veselkov.811@gmail.com>
2023-06-06 14:44:48 +0600

    static files / url rewrite

--- Program.cs
+++ Program.cs
@@ -1,28 +1,36 @@
+using Microsoft.AspNetCore.Rewrite;
 using SmartEdu.FileLogger;
 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.RegisterModules();
-
+// json config
 builder.Configuration.AddJsonFile("Config/appsettings.json");
 
+// file logger
 builder.Logging.AddFile(builder.Configuration["Logging:LoggerFileName"]!);
+
+// modules services
+builder.Services.RegisterModules();
 
 
 
 var app = builder.Build();
 
-app.UseStatusCodePagesWithReExecute("/error/{0}");
+// connect URL-rewriter
+var options = new RewriteOptions()
+            .AddRewrite("^$", "documents/Main_page.html", false);
 
+app.UseRewriter(options);
+
+// modules endpoints
 app.MapEndpoints();
 
+// error handling
+app.UseStatusCodePagesWithReExecute("/error/{0}");
 
+// static files
+app.UseStaticFiles();
 
-app.Map("/", () => 
-{
-    
-});
-
-app.Run("http://localhost:228");
+app.Run("https://localhost:228");
