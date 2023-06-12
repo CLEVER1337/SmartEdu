d02600d11596817d8e103f540a28acbbdb8f00fc
hih0lp <mrlibobo@yandex.ru>
2023-06-12 12:38:54 +0600

    formSender remaster

--- TUTOR_REGISTRATION/formSender.js
+++ TUTOR_REGISTRATION/formSender.js
@@ -1,58 +1,62 @@
-function serializeForm(formNode) {
-    const { elements } = formNode
+// function serializeForm(formNode) {
+//     const { elements } = formNode
   
-    const data = Array.from(elements)
-      .map((element) => {
-        const { name } = element
-        const value = element.value
+//     const data = Array.from(elements)
+//       .map((element) => {
+//         const { name } = element
+//         const value = element.value
   
-        return { name, value }
-      })
-      .filter((item) => !!item.name)
+//         return { name, value }
+//       })
+//       .filter((item) => !!item.name)
   
-    console.log(data)
+//     console.log(data)
+// }
+
+
+// async function sendData(data) {
+//     return await fetch('https://localhost:228/registration/tutor', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'multipart/form-data' },
+//       body: data
+//     })
+// }
+
+
+// async function handleFormSubmit(event) {
+//     event.preventDefault()
+//     const data = serializeForm(event.target)
+  
+  
+//     const { status, error } = await sendData(data)
+  
+//     if (status === 200) {
+//       onSuccess(event.target)
+//     } else {
+//       onError(error)
+//     }
+// }
+
+
+
+
+async function sendDataToServer(data){
+  return await fetch('https://localhost:228/', {
+    method: 'POST',
+    headers: {"Accept": "application/json", "Content-Type": "application/json"},
+    body: new FormData(Form)
+  })
+
 }
 
-function onSuccess(formNode) {
-    alert('Ваша заявка отправлена!')
-    formNode.classList.toggle('hidden')
-  }
 
-async function sendData(data) {
-    return await fetch('https://localhost:228/registration/tutor', {
-      method: 'POST',
-      headers: { 'Content-Type': 'multipart/form-data' },
-      body: data,
-    })
+const Form = document.getElementById('Form')
+
+
+Form.onsubmit = async (e) => {
+  e.preventDefault()
+  sendDataToServer(Form)
 }
 
-function onError(error) {
-    alert(error.message)
-  }
-
-function toggleLoader() {
-    const loader = document.getElementById('loader')
-    loader.classList.toggle('hidden')
-  }
-  
-async function handleFormSubmit(event) {
-    event.preventDefault()
-    const data = serializeForm(event.target)
-  
-    toggleLoader()
-  
-    const { status, error } = await sendData(data)
-    toggleLoader()
-  
-    if (status === 200) {
-      onSuccess(event.target)
-    } else {
-      onError(error)
-    }
-}
-
-
-
-const applicantForm = document.getElementById('aboba')
-applicantForm.addEventListener('submit', handleFormSubmit)
+// Form.addEventListener('submit', handleFormSubmit)
 
