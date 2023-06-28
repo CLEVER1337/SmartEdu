5c000bd674b90e99ef7fab403a0eab0643b3450b
hih0lp <mrlibobo@yandex.ru>
2023-06-28 20:30:47 +0600

    add a Personal account page

---
+++ wwwroot/scripts/CreatingCoursesTest.js
@@ -0,0 +1,106 @@
+// function addNewCourse(){
+//     var newCourse =  document.createElement('div')
+//     Course.classList.add('wrapperCourse_1')
+//     document.querySelector('.Wrapper').appendChild(Course)
+// document.body.append(new)
+// }
+
+// const newCourse = null
+// newCourse.addEventListener('click', addNewCourse())
+
+
+// const createButton = document.getElementById('button')
+// createButton.appendChild(newCourse)
+
+
+
+// var button = document.getElementById('heading')
+// button.onsubmit = addNewCourse()
+
+// const button = document.getElementById('button')
+// button.addEventListener('click', addNewCourse())
+
+
+
+// function aboba(){
+//     const newCourse = document.createElement('div')
+//     newCourse.className = "wrapperCourse"
+//     document.querySelector('.Wrapper').appendChild(newCourse)
+
+
+//     // create a profile photo container
+//     // const profilePhotoWrapper = document.createElement('div')
+//     // profilePhotoWrapper.className = "AvatarkaWrapper"
+//     // document.querySelector('.wrapperCourse').appendChild(profilePhotoWrapper)
+
+
+//     //create a profile photo container
+//     const profilePhotoContainer = document.createElement('div')
+//     profilePhotoContainer.className = "AvatarkaWrapper"
+//     document.querySelector('.wrapperCourse').appendChild(profilePhotoContainer)
+
+//     //create a profile photo
+//     const profilePhoto = document.createElement('div')
+//     profilePhoto.className = "Avatarka div"
+//     document.querySelector('.AvatarkaWrapper').appendChild(profilePhoto).after(wrapperCourse)
+// }
+
+// function add(){
+//     const Wrapper = document.querySelector('.Wrapper')
+
+//     const newwrapperCourse = document.createElement('div')
+//     newwrapperCourse.className = 'wrapperCourse'
+
+//     const newAvatarkaWrapper = document.createElement('div')
+//     newAvatarkaWrapper.className = 'AvatarkaWrapper'
+
+//     const newAvatarka = document.createElement('div')
+//     newAvatarka.className = 'Avatarka div'
+
+//     const newImage = document.createElement('div')
+//     newImage.className = 'Image div'
+
+//     const newImageIn = document.createElement('img')
+//     newImageIn.className = 'inputImage'
+//     newImageIn.src = "IconChemistryCourses.png"
+
+//     const newTask = document.createElement('div')
+//     newTask.className = 'Task div'
+//     newTask.innerText = 'Заданий 10'
+
+//     const newClass = document.createElement('div')
+//     newClass.className = 'Class div'
+//     newClass.innerText = 'Класс 7'
+
+//     const newRating = document.createElement('div')
+//     newRating.className = 'Rating div'
+//     newRating.innerText = 'Рейтинг'
+    
+
+//     const newCost = document.createElement('div')
+//     newCost.className = 'Cost div'
+//     newCost.innerText = 'Цена 5000'
+
+//     Wrapper.appendChild(newwrapperCourse)
+//     newwrapperCourse.appendChild(newAvatarkaWrapper)
+//     newAvatarkaWrapper.appendChild(newAvatarka)
+//     newwrapperCourse.appendChild(newImage)
+//     newImage.appendChild(newImageIn)
+//     newwrapperCourse.appendChild(newTask)
+//     newwrapperCourse.appendChild(newClass)
+//     newwrapperCourse.appendChild(newRating)
+//     newwrapperCourse.appendChild(newCost)
+    
+
+    
+// }
+
+
+
+
+// document.querySelector('button').addEventListener('click', add)
+
+
+
+
+
